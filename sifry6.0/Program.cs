611231efed59a95580c36e4e2150c1e9134b84de
Roman Hozak <roman.hozak.021@g.pslib.cz>
2023-04-26 13:28:01 +0200

    JJG

---
+++ sifry6.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+using (Aes myAes = Aes.Create())
+{
+    foreach (var x in myAes.Key)
+    {
+        Console.Write(String.Format("{0:x2} ", x));
+    }
+    foreach (var x in myAes.IV)
+    {
+        Console.Write(String.Format("{0:x2} ", x));
+    }
+}
